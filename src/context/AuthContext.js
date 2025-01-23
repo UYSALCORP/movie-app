6bb6d811c0f406b716b563d892e73570bfa175f6
UYSALCORP <uysalcorp@gmail.com>
2025-01-23 22:38:34 +0300

    login

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,7 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import React, { createContext } from "react";
 import { auth } from "../auth/firebase";
 import { toastSuccess } from "../helpers/ToastNotify";
@@ -7,20 +10,25 @@ import { useNavigate } from "react-router-dom";
 export const AuthContextt = createContext();
 
 const AuthContext = ({ children }) => {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
+  const createUser = async (email, password) => {
+    await createUserWithEmailAndPassword(auth, email, password);
+    toastSuccess("Register Completed!");
+    navigate("/");
+  };
 
-  const createUser=async(email, password)=>{
-  await createUserWithEmailAndPassword(auth, email, password);
-    toastSuccess("Register Completed!")
-    navigate("/")
-  }
+  const signInUser = async (email, password) => {
+    await signInWithEmailAndPassword(auth, email, password);
+    toastSuccess("Logined Succesfully!");
+    navigate("/");
+  };
 
-
-
-  return <AuthContextt.Provider value={{createUser}}>
-  {children}
-  </AuthContextt.Provider>;
+  return (
+    <AuthContextt.Provider value={{ createUser, signInUser }}>
+      {children}
+    </AuthContextt.Provider>
+  );
 };
 
 export default AuthContext;
