8f2cd2331edd88699e31da4cfda26b01a183cdca
UYSALCORP <uysalcorp@gmail.com>
2025-01-23 20:53:49 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import AppRouter from "./router/AppRouter"
+import { ToastContainer } from "react-toastify";
 
-import Register from "./pages/Register";
 
 const App = () => {
   return (
     <div className="dark:bg-[#23242a] min-h-screen">
-     <Register/>
+      <AppRouter/>
+      <ToastContainer/>
     </div>
   );
 };
